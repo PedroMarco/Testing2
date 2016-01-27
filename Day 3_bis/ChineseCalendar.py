042ac4fea5288de8c76a067bd8b8cee2307cbd51
pedromarco <pedrom28@gmail.com>
2016-01-27 19:00:07 +0000

    Chinese calendar test 1

---
+++ Day 3_bis/ChineseCalendar.py
@@ -0,0 +1,30 @@
+__author__ = 'acpb859'
+
+###use eval, remember to use eval, or it will be a simple string
+
+year = eval(input('enter an year '))
+
+if (year % 12) == 0:
+    print('monkey')
+elif (year % 12) == 1:
+    print('rooster')
+elif (year % 12) == 2:
+    print('dog')
+elif (year % 12) == 3:
+    print('pig')
+elif (year % 12) == 4:
+    print('rat')
+elif (year % 12) == 5:
+    print('ox')
+elif (year % 12) == 6:
+    print('tiger')
+elif (year % 12) == 7:
+    print('rabbit')
+elif (year % 12) == 8:
+    print('dragon')
+elif (year % 12) == 9:
+    print('snake')
+elif (year % 12) == 10:
+    print('horse')
+else:
+    print('sheep')
